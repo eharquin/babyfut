90227f52c0d6540c1181a596cc4f6059a7d992e6
Antoine Lima <antoine.lima@etu.utc.fr>
2018-10-22 21:33:50 +0200

    PiCam replay

--- com.py
+++ com.py
@@ -7,33 +7,32 @@ Created on Wed Apr 18 18:34:40 2018
 """
 
 import logging
-import autopy
 import serial
-from os.path import dirname, abspath, join, isfile as exists 
-from autopy.key import tap as PressKey, Code as KeyCode 
+from os.path import isfile as exists
+from autopy.key import tap as PressKey, Code as KeyCode
 from threading import Thread
 
 from player import Side
 
 class InputThread(Thread):
 	keyButtonBindings = [KeyCode.ESCAPE, KeyCode.UP_ARROW, KeyCode.LEFT_ARROW, KeyCode.RIGHT_ARROW, KeyCode.DOWN_ARROW, KeyCode.RETURN]
-	
+
 	def __init__(self, dispatcher, side):
 		Thread.__init__(self)
 		self.side = side
 		self.dispatcher = dispatcher
 		self.continueRunning = True
 		self.path = '/dev/ttyUSB0' if self.side==Side.Left else '/dev/ttyUSB1'
-		
+
 		if exists(self.path):
 			self.arduino = serial.Serial(self.path, 9600, timeout=1)
 		else:
 			raise RuntimeError('No arduino connected on the {} side'.format(self.side.name.lower()))
-	
+
 	def run(self):
 		while self.arduino.isOpen():
 			msg = self.arduino.readline()[:-1]
-			
+
 			if msg:
 				parsedMessage = self.parseMsg(msg)
 				if 'butn' in parsedMessage:
@@ -46,14 +45,14 @@ class InputThread(Thread):
 	def stop(self):
 		self.continueRunning = False
 		self.arduino.close()
-	
+
 	def sendKeyStroke(self, msg):
 		if 'butn' in msg:
 			button = int(msg['butn'])
 			print({button: ['ESCAPE', 'UP_ARROW', 'LEFT_ARROW', 'RIGHT_ARROW', 'DOWN_ARROW', 'RETURN'][button]})
 			key = InputThread.keyButtonBindings[button]
 			PressKey(key, [])
-	
+
 	def parseMsg(self, msg):
 		parts = msg.split(':')
 		return {parts[0]: parts[1], 'source': self.side}
--- replay.py
+++ replay.py
@@ -7,6 +7,10 @@ Created on Wed Apr 18 18:34:40 2018
 """
 
 import os
+from threading import Event
+from multiprocessing import Process, Lock
+
+from main import MainWin
 from settings import Settings
 
 onRasp = os.uname()[1] == 'raspberrypi'
@@ -15,8 +19,17 @@ if onRasp:
 	import picamera
 
 
+class LockableValue():
+	def __init__(self, value):
+		self.value = value
+		self.mutex = Lock()
+
 class Replay():
-	def __init__(self):
+	def __init__(self, side):
+		self.recording = LockableValue(False)
+		self.replayPath = MainWin.getContent('Replay {}.mp4'.format(side.name))
+		self.stopped = Event()
+
 		if onRasp:
 			self.cam = picamera.PiCamera()
 			self.cam.resolution = Settings['picam.resolution']
@@ -24,23 +37,36 @@ class Replay():
 			self.cam.hflip = Settings['picam.hflip']
 			self.cam.vflip = Settings['picam.vflip']
 			self.format = Settings['picam.format']
-			self.continue_recording = False
 			self.stream = picamera.PiCameraCircularIO(self.cam, seconds=Settings['replay.duration'])
-	
+
+	def start_recording(self):
+		if not onRasp:
+			self.stopped.set()
+		else:
+			self.recording.value = True
+			self.stopped.clear()
+			self.capture_process = Process(target=self.__capture)
+			self.capture_process.start()
+
+	def stop_recording(self):
+		self.recording.val = True
+		self.stopped.wait(timeout=2.0)
+		return self.replayPath
+
 	def capture(self, fileToSave):
 		if onRasp:
 			self.cam.start_recording(self.stream, self.format)
-			self.continue_recording = True
-			
+
 			try:
-				while self.continue_recording:
+				recording = self.recording.val
+
+				while recording:
 					self.cam.wait_recording(1)
+					recording = self.recording.val
 			finally:
 				self.cam.stop_recording()
-				
-			self.stream.copy_to(fileToSave)
+
+			self.stopped.set()
+			self.stream.copy_to(self.replayPath)
 			self.cam.close()
 			self.stream.close()
-
-	def stop():
-		self.continue_recording = False
