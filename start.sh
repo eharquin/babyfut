bad363b2e3e9464b4195f7c78c11b9b2e4bb8e36
adam <adam.soimansoib@etu-utc.fr>
2022-04-23 19:33:00 +0200

    Creating a script to start application

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+gnome-terminal --title=Slave1 --tab -- sh -c "./devtools.sh slave"
+gnome-terminal --title=Slave2 --tab -- sh -c "./devtools.sh slave"
+gnome-terminal --title=Master --tab -- sh -c "./devtools.sh master"
