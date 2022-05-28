d455c7f5c73432f23a58e443551450df7a1c74d7
Adam <adam.soimansoib@etu.utc.fr>
2022-05-28 22:51:36 +0200

    Add source venv/bin/activate to script.

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+source venv/bin/activate;
 gnome-terminal --title=Slave1 --tab -- sh -c "./devtools.sh slave"
 gnome-terminal --title=Slave2 --tab -- sh -c "./devtools.sh slave"
 gnome-terminal --title=Master --tab -- sh -c "./devtools.sh master"
